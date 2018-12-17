b09b9805c12985c0e99d24a08c7fde422e3f2ab9
Connor Zanin <cnnrznn@gmail.com>
2018-12-17 16:38:51 -0500

    db skeleton

--- jepsen.etcdemo/src/jepsen/etcdemo.clj
+++ jepsen.etcdemo/src/jepsen/etcdemo.clj
@@ -1,13 +1,32 @@
 (ns jepsen.etcdemo
-  (:require [jepsen.cli :as cli]
-            [jepsen.tests :as tests]))
+  (:require [clojure.tools.logging :refer :all]
+            [clojure.string :as str]
+            [jepsen [cli :as cli]
+                    [control :as ctrl]
+                    [db :as db]
+                    [tests :as tests]]
+            [jepsen.control.util :as cu]
+            [jepsen.os.debian :as debian]))
+
+(defn db
+  "Etcd DB for a particular version."
+  [version]
+  (reify db/DB
+    (setup! [_ test node]
+      (info node "installing etcd" version))
+
+    (teardown! [_ test node]
+      (info node "tearing down etcd"))))
 
 (defn etcd-test
   "Given an options map from the command line runner (e.g. :nodes, :ssh,
   :concurrency, ...), constructs a test map."
   [opts]
   (merge tests/noop-test
-         opts))
+         opts
+         {:name "etcd"
+          :os debian/os
+          :db (db "v3.1.5")}))
 
 (defn -main
   "A very good place to start."
